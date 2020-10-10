8a0df8cd70c6f8c57da41323d5082d6d007a427f
CodeSpaceIndica <72309821+CodeSpaceIndica@users.noreply.github.com>
2020-10-10 12:50:11 +0530

    Functions that were explained in supplement videos
    codespace_api.js will contain a list of all the functions/classes/utilities that were explained in the supplement videos.

---
+++ codespace_api.js
@@ -0,0 +1,13 @@
+
+/**
+ * Maps a number of a given input range to a number of the output range.
+ * 
+ * @param {*} inputNum 
+ * @param {*} minInput 
+ * @param {*} maxInput 
+ * @param {*} minOutput 
+ * @param {*} maxOutput 
+ */
+function map(inputNum, minInput, maxInput, minOutput, maxOutput) {
+    return (inputNum - minInput) * (maxOutput - minOutput) / (maxInput - minInput) + minOutput;
+}
