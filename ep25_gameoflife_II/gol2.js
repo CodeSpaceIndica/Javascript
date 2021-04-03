bf7bdbbe0b67c0f233ff47683d8151aeaf851dfd
Code Space <72309821+CodeSpaceIndica@users.noreply.github.com>
2021-04-03 22:55:04 +0530

    Buunch of changes
    1. Made particle size and interaction distance proportional to canvas width
    2. Fixed issue with distance
    3. Added reset functionality

--- ep25_gameoflife_II/gol2.js
+++ ep25_gameoflife_II/gol2.js
@@ -21,14 +21,34 @@ var PARTICLE_INTERACTION = [];
 
 var particles = new Array(MAX_PARTICLES);
 
+let reqAnimID;
+
 function init() {
     //Init the canvas
     var canvasElement = document.getElementById("gol2Canvas");
+    resizeCanvas(canvasElement, false);
 
     ctx = canvasElement.getContext("2d");
 
     width = canvasElement.width;
     height = canvasElement.height;
+
+    //Make the particle sizes and the minimum and maximum interation distance 
+    //a proportion of the width of the canvas.
+    MIN_INTERACT_DIST = width / 100;
+    MAX_INTERACT_DIST = MIN_INTERACT_DIST * 3;
+    PARTICLE_SIZE = width / 222.22;
+    PARTICLE_DIAM = PARTICLE_SIZE * 2;
+
+    startAnim();
+}
+
+function reset() {
+    cancelAnimationFrame(reqAnimID); 
+
+    PARTICLE_INTERACTION = [];
+
+    particles = new Array(MAX_PARTICLES);
 
     startAnim();
 }
@@ -47,8 +67,8 @@ function startAnim() {
             }
 
             randomNum = parseInt( randomBetween(1, 4) );
-            randomNum = randomNum > 4 ? 3 : randomNum;
-            let distance = randomNum * 10;
+            randomNum = randomNum > 3 ? 3 : randomNum;
+            let distance = randomNum * MIN_INTERACT_DIST;
 
             let vv = {
                 TYPE: type,
