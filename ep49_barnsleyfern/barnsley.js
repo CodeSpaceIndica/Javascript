ceeb807a46343f14fdc9978d48cc9108a7e33c58
The Big Int <72309821+CodeSpaceIndica@users.noreply.github.com>
2022-02-05 20:28:24 +0530

    Barnsley Fern

---
+++ ep49_barnsleyfern/barnsley.js
@@ -0,0 +1,75 @@
+//Constants
+var MAX_POINTS = 10000;
+
+//GLOBALS
+var ctx;
+var width, height;
+
+var ctr = 0;
+var x, y;
+
+window.addEventListener("load", (event) => {
+    let theCanvas = document.getElementById("aCanvas");
+    resizeCanvas(theCanvas, false);
+    width = theCanvas.width;
+    height = theCanvas.height;
+
+    ctx = theCanvas.getContext("2d");
+
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.rect(0, 0, width, height);
+    ctx.fill();
+
+    ctx.fillStyle = "#00FF00";
+
+    ctr = 0;
+    x = 0;
+    y = 0;
+
+    plotBarnsleyFern();
+});
+
+function plotBarnsleyFern() {
+    for(let i=0; i<100; i++) {
+        let nextX, nextY;
+        let rnd = Math.random();
+        if( rnd < 0.01 ) {
+            nextX = 0;
+            nextY = 0.16 * y;
+        }
+        else if( rnd < 0.86 ) {
+            nextX =  0.85 * x + 0.04 * y;
+            nextY = -0.04 * x + 0.85 * y + 1.6;
+        }
+        else if( rnd < 0.93 ) {
+            nextX = 0.20 * x - 0.26 * y;
+            nextY = 0.23 * x + 0.22 * y + 1.6;
+        }
+        else {
+            nextX = -0.15 * x + 0.28 * y;
+            nextY =  0.26 * x + 0.24 * y + 0.44;
+        }
+
+        //−2.1820 < x < 2.6558 and 0 ≤ y < 9.9983.
+        let pX = map(x, -2.1820, 2.6558, 0, width);
+        let pY = map(y, 0, 9.9983, height, 0);
+
+        let h = map(y, 0, 9.9983, 0, 360);
+        ctx.fillStyle = "hsl(" + h + ",100%, 50%)";
+        ctx.beginPath();
+        ctx.rect(pX, pY, 2, 2);
+        ctx.fill();
+
+        x = nextX;
+        y = nextY;
+    }
+
+    ctr++;
+    if( ctr > MAX_POINTS ) {
+        console.log("Done");
+        return;
+    }
+
+    setTimeout(plotBarnsleyFern, 1);
+}
